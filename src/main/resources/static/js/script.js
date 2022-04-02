f9f4dc00e2620cb5712e21bf341e57eb677ca065
Daniel Żyła <dzmsgbox@gmail.com>
2022-04-02 02:07:32 +0200

    script.js refactored, set time added for browser tab visibility

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,15 +1,26 @@
-let time = new Date();
-let periodPerSecond = (time.getSeconds() + 1) * 6;
-const clockHandSecond = document.querySelector(".clock-hand.seconds");
-let periodPerMinute = time.getMinutes() * 6  + time.getSeconds() / 60 * 6;
-const clockHandMinutes = document.querySelector(".clock-hand.minutes");
+let time;
+let periodPerSecond;
+let periodPerMinute;
 let periodPerHour;
-if (time.getHours > 12) {
-    periodPerHour = (time.getHours() - 12 + time.getMinutes() / 60) * 30;
-} else periodPerHour = (time.getHours() + time.getMinutes() / 60) * 30;
+const clockHandSecond = document.querySelector(".clock-hand.seconds");
+const clockHandMinutes = document.querySelector(".clock-hand.minutes");
 const clockHandHours = document.querySelector(".clock-hand.hours");
 const clockShieldHandle = document.querySelector("#clock-shield");
 const clockDigitalHandle = document.querySelector("#clock-whole");
+
+function setTimeInElements() {
+    time = new Date();
+    periodPerSecond = time.getSeconds() * 6;
+    periodPerMinute = time.getMinutes() * 6  + time.getSeconds() / 60 * 6;
+    if (time.getHours > 12) {
+        periodPerHour = (time.getHours() - 12 + time.getMinutes() / 60) * 30;
+    } else periodPerHour = (time.getHours() + time.getMinutes() / 60) * 30;
+    rotateSecondHand();
+    rotateMinuteHand();
+    rotateHourHand();
+}
+
+setTimeInElements();
 
 function rotateSecondHand() {
     periodPerSecond += 6;
@@ -38,29 +49,49 @@ function rotate(rotatePeriod) {
 }
 
 window.onload = function() {
-    setInterval(function() {
 
-        time = new Date();
-        
-        let day = time.getDate();
-        let month = time.getMonth() + 1;
-        let year = time.getFullYear();
-        let hours = time.getHours();
-        let minutes = time.getMinutes();
-        let seconds = time.getSeconds();
+    let intervalID;
 
-        if(day < 10) day = "0" + day;
-        if(month < 10) month = "0" + month;
-        if(hours < 10) hours = "0" + hours;
-        if(minutes < 10) minutes = "0" + minutes;
-        if(seconds < 10) seconds = "0" + seconds % 360;
+    function runClock() {
+        if(!intervalID) {
+            intervalID = setInterval(function() {
 
-        document.getElementById("clock-digital").innerHTML 
-        = day + "." + month + "." + year + " - " + hours + ":" + minutes + ":" + seconds;
+                time = new Date();
 
-        rotateSecondHand();
-        rotateMinuteHand();
-        rotateHourHand();
+                let day = time.getDate();
+                let month = time.getMonth() + 1;
+                let year = time.getFullYear();
+                let hours = time.getHours();
+                let minutes = time.getMinutes();
+                let seconds = time.getSeconds();
 
-    }, 1000);
+                if(day < 10) day = "0" + day;
+                if(month < 10) month = "0" + month;
+                if(hours < 10) hours = "0" + hours;
+                if(minutes < 10) minutes = "0" + minutes;
+                if(seconds < 10) seconds = "0" + seconds % 360;
+
+                document.getElementById("clock-digital").innerHTML
+                = day + "." + month + "." + year + " - " + hours + ":" + minutes + ":" + seconds;
+
+                rotateSecondHand();
+                rotateMinuteHand();
+                rotateHourHand();
+
+            }, 1000);
+        }
+    }
+
+    runClock();
+
+    document.addEventListener('visibilitychange', function() {
+        if(document.hidden) {
+            clearInterval(intervalID);
+            intervalID = null;
+        }
+        else {
+            setTimeInElements();
+            runClock();
+        }
+    });
 }
